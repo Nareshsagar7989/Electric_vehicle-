5f23cbd6ee550f33edcf8c4991ecd11815f49912
Nareshsagar7989 <kondikantinareshsagar7989@gmail.com>
2025-07-20 08:54:18 +0530

    updated code4

--- code.py
+++ code.py
@@ -7,4 +7,4 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_scor
